8873d7b6fdae12b21d6abb07e4b78f13974bf702
alex <alexisnotonffire@users.noreply.github.com>
2020-03-01 13:18:24 +0000

    expand list

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import TaskList from './TaskList';
-
-const taskList = [
-  { name: "do me" },
-  { name: "touch me" },
-]
+import React from 'react'; 
+import TaskListManager from './TaskListManager';
 
 function App() {
+  const taskList = {
+    tasks: [
+      {name: 'hoops'},
+      {name: 'loops'},
+    ]
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         byte me
       </header>
-      <TaskList tasks={taskList}></TaskList>
+      <TaskListManager taskList={ taskList } />
     </div>
   );
 }
